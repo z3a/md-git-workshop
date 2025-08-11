4c4817ef6b3962264c950bdf2785e8eb46aed742
Gabriel Zea <zea@gbrl.in>
2025-08-11 13:07:16 +0000

    fix user

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#######
+# Config ZSH
+#######
+# powerline fonts for zsh theme
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd .. && rm -rf fonts
+
+# oh-my-zsh plugins
+zsh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k'
+cp .devcontainer/dotfiles/.zshrc ~
+cp .devcontainer/dotfiles/.p10k.zsh ~
+
+
+#######
+# Done
+#######
